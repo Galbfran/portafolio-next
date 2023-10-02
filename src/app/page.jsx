9d0540cefa7ee92c7914856e6d502810960dc38b
Galbfran <francogalbiati984@gmail.com>
2023-10-02 20:40:01 -0300

    subo nav y header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,10 @@
-
+import Header from "@/components/Header"
 
 
 export default function Home() {
   return (
-    <main >
-     hola mundo
-     <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">hola</button>
+    <main className="container" >
+      <Header/>
 
     </main>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
-            <div className="container">
+        <nav className="navbar navbar-expand-lg bg-black " data-bs-theme="dark">
+            <div className="container d-flex">
                 <Link href="/" className="navbar-brand">Galbiati Franco</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
